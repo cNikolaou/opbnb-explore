f63e30b3d11142c52213f37170fe5c9910983f6f
Christos Nikolaou <me@christosnikolaou.org>
2024-02-29 09:01:31 +0000

    Add main script to manage Observer and extraction threads

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+
+import settings
+from extract import extract_data
+from transform import set_csv_file_transformer
+
+if __name__ == "__main__":
+
+    # create the data directory if it doesn't exist
+    os.makedirs(settings.DATA_DIR, exist_ok=True)
+
+    # set the Observer that handles the CSV file transformation
+    observer = set_csv_file_transformer(settings.DATA_DIR)
+    observer.start()
+
+    # start the data retrieval threads
+    extract_data(
+        end_block_number=10,
+        block_batch_size=10,
+        output_dir=settings.DATA_DIR,
+        max_workers=1,
+        concurrent_requests=1,
+    )
+
+    extract_data(
+        start_block_number=17442490,
+        end_block_number=17442500,
+        block_batch_size=10,
+        output_dir=settings.DATA_DIR,
+        max_workers=2,
+        concurrent_requests=1,
+    )
+
+    # wait for all the file events in the queue to be processed by the Observer
+    observer.event_queue.join()
