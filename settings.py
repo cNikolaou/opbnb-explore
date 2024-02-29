973c31e952bc0908c7ab29ae5fa72b89b60800ea
Christos Nikolaou <me@christosnikolaou.org>
2024-02-29 14:10:30 +0000

    Add logging format config in `settings.py`

--- settings.py
+++ settings.py
@@ -1,7 +1,13 @@
-from dotenv import load_dotenv
 import os
+import logging
+from dotenv import load_dotenv
 
 load_dotenv()
 
 PROVIDER_URI = os.getenv("PROVIDER_URI", "https://opbnb-mainnet-rpc.bnbchain.org")
 DATA_DIR = os.getenv("DATA_DIR", "data")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s: %(levelname)s/%(name)s] %(message)s",
+)
