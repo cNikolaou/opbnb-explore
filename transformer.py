5ccc19fbb4d6968a081ff545adcd227b3419e094
Christos Nikolaou <me@christosnikolaou.org>
2024-03-03 16:24:50 +0000

    Add function to run dir transformation manually
    Add function to run dir transformation manually without
    depending on the `CSVTransformHandler` running the transformation.

--- transformer.py
+++ transformer.py
@@ -11,11 +11,53 @@ from watchdog.events import (
     EVENT_TYPE_MODIFIED,
 )
 
+import settings
+
 from utils import to_relative_path
-from transform import change_dir, transform_blocks_data, transform_transactions_data
+from transform import (
+    change_dir,
+    transform_blocks_data,
+    transform_transactions_data,
+    transform_tokens_data,
+)
 
 
 logger = logging.getLogger("CSVTransformHandler")
+
+
+def transform_file(path: Path, cwd: str):
+
+    parent_dirs = path.parent.parts
+
+    # If the file is located within a `blocks` directory
+    if "blocks" in parent_dirs:
+        replacements = {"blocks": "blocks_transformed"}
+        new_path = change_dir(path, replacements)
+        logger.info(
+            f"Transform file `{to_relative_path(path, cwd)}` "
+            f"to `{to_relative_path(new_path, cwd)}`"
+        )
+        transform_blocks_data(str(path), str(new_path))
+
+    # If the file is located within a `transactions` directory
+    elif "transactions" in parent_dirs:
+        replacements = {"transactions": "transactions_transformed"}
+        new_path = change_dir(path, replacements)
+        logger.info(
+            f"Transform file `{to_relative_path(path, cwd)}` "
+            f"to `{to_relative_path(new_path, cwd)}`"
+        )
+        transform_transactions_data(str(path), str(new_path))
+
+    # If the file is located within a `tokens` directory
+    elif "tokens" in parent_dirs:
+        replacements = {"tokens": "tokens_transformed"}
+        new_path = change_dir(path, replacements)
+        logger.info(
+            f"Transform file `{to_relative_path(path, cwd)}` "
+            f"to `{to_relative_path(new_path, cwd)}`"
+        )
+        transform_tokens_data(str(path), str(new_path))
 
 
 class CSVTransformHandler(FileSystemEventHandler):
@@ -45,25 +87,7 @@ class CSVTransformHandler(FileSystemEventHandler):
                 if self._file_path_queue:
                     self._file_path_queue.put(path)
 
-                # If the file is located within a `blocks` directory
-                if "blocks" in parent_dirs:
-                    replacements = {"blocks": "blocks_transformed"}
-                    new_path = change_dir(path, replacements)
-                    logger.info(
-                        f"Transform file `{to_relative_path(path, self._cwd)}` "
-                        f"to `{to_relative_path(new_path, self._cwd)}`"
-                    )
-                    transform_blocks_data(str(path), str(new_path))
-
-                # If the file is located within a `transactions` directory
-                elif "transactions" in parent_dirs:
-                    replacements = {"transactions": "transactions_transformed"}
-                    new_path = change_dir(path, replacements)
-                    logger.info(
-                        f"Transform file `{to_relative_path(path, self._cwd)}` "
-                        f"to `{to_relative_path(new_path, self._cwd)}`"
-                    )
-                    transform_transactions_data(str(path), str(new_path))
+                transform_file(path, self._cwd)
 
 
 def set_csv_file_transformer(data_dir: str, file_path_queue: Optional[Queue] = None):
@@ -73,3 +97,50 @@ def set_csv_file_transformer(data_dir: str, file_path_queue: Optional[Queue] = N
     observer.schedule(event_handler, data_dir, recursive=True)
 
     return observer
+
+
+def transform_files_in(data_root: str, data_dir: str):
+    """
+    Transform files in a directory.
+    """
+
+    data_path = Path(data_root) / data_dir
+
+    print(data_path.absolute())
+
+    for start_dir in data_path.iterdir():
+
+        # ignore directories that do not start with "start_block"
+        if not start_dir.stem.startswith("start_block"):
+            logger.warning(f"Non-data directory {start_dir}")
+            continue
+
+        # get the start block number from the directory name
+        start_block_num = int(start_dir.stem.split("=")[1])
+
+        # expect to have only one subdirectory so only one iteration is allowed
+        iterated = False
+
+        for end_dir in start_dir.iterdir():
+
+            # ignore directories that do not start with "end_block"
+            if not end_dir.stem.startswith("end_block"):
+                logger.warning(f"Non-data directory {end_dir}")
+                continue
+
+            # check if we have already iterated
+            if iterated:
+                raise Exception(
+                    f"More that one data files from a single start "
+                    f"block in: {str(start_dir)}"
+                )
+
+            # get the end block number from the directory name
+            end_block_num = int(end_dir.stem.split("=")[1])
+
+            file_path = end_dir / f"{data_dir}_{start_block_num}_{end_block_num}.csv"
+
+            transform_file(file_path, data_root)
+
+            # only one iteration is valid
+            iterated = True
