657d2f556f1ed66eeeb62e6e69a327272a41e3c0
Christos Nikolaou <me@christosnikolaou.org>
2024-03-04 13:34:20 +0000

    Use `DBLoadHandler` in `main.py` script

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import os
 from queue import Queue
+import time
 
 import settings
 from extract import extract_data
 from transformer import set_csv_file_transformer
-from uploader import UploadHandler
+
+# from uploader import UploadHandler
+
+from database.utils import create_tables
+from db_loader import DBLoadHandler
+
 
 if __name__ == "__main__":
 
@@ -18,22 +24,27 @@ if __name__ == "__main__":
     observer = set_csv_file_transformer(settings.DATA_DIR, file_path_queue)
     observer.start()
 
-    uploader = UploadHandler(file_path_queue, 5)
-    uploader.start()
+    # uploader = UploadHandler(file_path_queue, 10)
+    # uploader.start()
 
-    # start the data retrieval threads
+    create_tables()
+    db_loader = DBLoadHandler(file_path_queue, max_workers=1, retain_origin_files=True)
+    db_loader.start()
+
+    # # start the data retrieval threads
+    # extract_data(
+    #     end_block_number=10,
+    #     block_batch_size=10,
+    #     output_dir=settings.DATA_DIR,
+    #     max_workers=1,
+    #     concurrent_requests=1,
+    # )
+
+    start = time.time()
     extract_data(
-        end_block_number=10,
+        start_block_number=15002737,
+        end_block_number=15002746,
         block_batch_size=10,
-        output_dir=settings.DATA_DIR,
-        max_workers=1,
-        concurrent_requests=1,
-    )
-
-    extract_data(
-        start_block_number=17442490,
-        end_block_number=17442590,
-        block_batch_size=20,
         output_dir=settings.DATA_DIR,
         max_workers=5,
         concurrent_requests=2,
@@ -42,7 +53,13 @@ if __name__ == "__main__":
     # wait for all the file events in the queue to be processed by the Observer
     # and to be added to the `file_path_queue`
     observer.event_queue.join()
+    end = time.time()
+
+    print("Total time:", end - start)
 
     # wait for the files queued on the uploader to be processed
-    uploader.wait_for_uploads()
-    uploader.stop()
+    # uploader.wait_for_uploads()
+    # uploader.stop()
+
+    # wait for the files queued on the db_loader to be processed
+    db_loader.stop()
